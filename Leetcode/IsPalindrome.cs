f8859394f43afb1b80cc90dc313d516134097d39
NITRO\smatveev <matv33v@gmail.com>
2021-02-25 12:46:29 +0300

    Palindrome task for linked list ref: https://leetcode.com/problems/palindrome-linked-list/

---
+++ Leetcode/IsPalindrome.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Leetcode
+{
+    static class Palindrome
+    {
+        public static bool IsPalindrome(ListNode head)
+        {
+            if (head == null) return true;
+
+            ListNode next = head;
+            ListNode nextx2 = head;
+
+            //calc the middle of list
+            while (nextx2.next != null && nextx2.next.next != null)
+            {
+                next = next.next;
+                nextx2 = nextx2.next.next;
+            }
+
+            // reverse
+            ListNode cur = next.next;
+            ListNode prev = null;
+            while (cur != null)
+            {
+                var tmp = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = tmp;
+            }
+
+            //checking by pairs
+            while (prev != null)
+            {
+                if (head.val != prev.val) return false;
+                prev = prev.next;
+                head = head.next;
+            }
+            return true;
+        }
+    }
+}
