51aa62bb6e7102231d8d78d4319d1981bc168cc9
NITRO\smatveev <matv33v@gmail.com>
2021-03-02 21:20:34 +0300

    Time conversion problem

---
+++ Leetcode/Problems/TimeConvertion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Problems
+{
+    class TimeConvertion
+    {
+        //test
+        // timeConversion("12:45:54PM");
+
+        static string timeConversion(string s)
+        {
+            /*
+             * Write your code here.
+             */
+            string hours;
+
+            char[] arr = s.ToCharArray();
+
+            if (arr[arr.Length - 2] == 'P')
+            {
+                hours = (12 + int.Parse(s.Substring(0, 2))).ToString();
+                if (hours == 24.ToString())
+                {
+                    hours = "12";
+                }
+            }
+
+            else
+            {
+                int h = int.Parse(s.Substring(0, 2));
+                if (h < 10)
+                    hours = string.Concat("0", h.ToString());
+                else if (h == 12)
+                    hours = "00";
+                else
+                    hours = h.ToString();
+            }
+
+            return string.Concat(hours, s.Substring(2, 6));
+        }
+
+    }
+}
