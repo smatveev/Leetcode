8aa27fe9551c202282c382dacfe682887ad5a4e8
NITRO\smatveev <matv33v@gmail.com>
2021-03-16 21:35:18 +0300

    Solution for problem - https://leetcode.com/problems/rectangle-overlap

---
+++ Leetcode/Problems/IsRectangleOverlap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Leetcode.Problems
+{
+    /*
+     solution for the problem https://leetcode.com/problems/rectangle-overlap
+     */
+    class IsRectangleOverlap
+    {
+        public static bool Solution(int[] rec1, int[] rec2)
+        {
+            return (Math.Min(rec1[2], rec2[2]) > Math.Max(rec1[0], rec2[0]) &&
+                    Math.Min(rec1[3], rec2[3]) > Math.Max(rec1[1], rec2[1]));
+        }
+    }
+}
