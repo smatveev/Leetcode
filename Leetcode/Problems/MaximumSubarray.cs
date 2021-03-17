d4472307a07bd4e5c82460ae475b58b6f72ece6b
NITRO\smatveev <matv33v@gmail.com>
2021-03-17 13:54:41 +0300

    solution for problem - https://leetcode.com/problems/maximum-subarray/

---
+++ Leetcode/Problems/MaximumSubarray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Problems
+{
+    /*
+     * solution for problem - https://leetcode.com/problems/maximum-subarray/
+     */
+    class MaximumSubarray
+    {
+        public static int Solution(int[] nums)
+        {
+            int curSum = nums[0];
+            int maxSum = curSum;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                curSum = Math.Max(nums[i], curSum + nums[i]);
+                maxSum = Math.Max(curSum, maxSum);
+            }
+
+            return maxSum;
+        }
+    }
+}
