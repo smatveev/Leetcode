4bf9b043d5b37b288ad8a24eda53b32b06007d1b
NITRO\smatveev <matv33v@gmail.com>
2021-04-02 17:29:56 +0300

    solution for https://leetcode.com/problems/top-k-frequent-elements/

---
+++ Leetcode/Problems/TopKFrequent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Problems
+{
+    /*
+     * solution for https://leetcode.com/problems/top-k-frequent-elements/
+     */
+    static class TopKFrequent
+    {
+        public static int[] Solution(int[] nums, int k)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            foreach (int n in nums)
+            {
+                if (!map.ContainsKey(n))
+                    map.Add(n, 0);
+
+                map[n]++;
+            }
+
+            return map.OrderByDescending(x => x.Value).Select(x => x.Key).Take(k).ToList<int>().ToArray();
+        }
+    }
+}
