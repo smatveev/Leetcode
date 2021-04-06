2270107e4ba05ae74fbb4016804576480e72b441
NITRO\smatveev <matv33v@gmail.com>
2021-04-06 21:07:20 +0300

    https://leetcode.com/problems/minimum-operations-to-make-array-equal/

---
+++ Leetcode/Problems/MinimumOperationsToMakeArrayEqual.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Problems
+{
+    /*
+     * https://leetcode.com/problems/minimum-operations-to-make-array-equal/
+     */
+    static class MinimumOperationsToMakeArrayEqual
+    {
+        static public int Foo(int n)
+        {
+            int res = 0;
+            while (n > 0)
+            {
+                res += n - 1;
+                n = n - 2;
+            }
+            return res;
+        }
+    }
+}
